873d499bb4e29fde20704a07c3a5594669f824ef
Americo <ame.zuz@gmail.com>
2018-11-22 18:25:36 -0800

    Complete sample app and add some suggested features

--- socket-io/index.js
+++ socket-io/index.js
@@ -7,12 +7,29 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('a user disconnected');
+  io.emit('chat message', 'New user');
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    io.emit('user left', 'User left');
   });
 });
 
 http.listen(3000, function() {
-  console.log('Listening on localhost:3000');
+  console.log('Listening on http://localhost:3000');
 });
+
+/*
+TODO:
+- [DONE] Broadcast a message to connected users when someone connects 
+  or disconnects.
+- Add support for nicknames.
+- Don’t send the same message to the user that sent it himself. Instead, 
+  append the message directly as soon as he presses enter.
+- Display user's own messages to the right, other user messages to the
+  left, system messages in the middle.
+- Add “{user} is typing” functionality.
+- Show who’s online.
+- Add private messaging.
+*/
