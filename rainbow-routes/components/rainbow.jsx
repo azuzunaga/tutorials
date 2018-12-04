81835516b8d037321daccdc6fd09717541142565
Americo <ame.zuz@gmail.com>
2018-12-03 23:33:30 -0800

    Add component routes

--- rainbow-routes/components/rainbow.jsx
+++ rainbow-routes/components/rainbow.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import {
-  Route,
-  Link,
-  NavLink
-} from 'react-router-dom';
+import { Route, Link, NavLink } from 'react-router-dom';
 
-import Red from './red';
-import Green from './green';
 import Blue from './blue';
+import Green from './green';
+import Indigo from './indigo';
+import Orange from './orange';
+import Red from './red';
 import Violet from './violet';
+import Yellow from './yellow';
 
 class Rainbow extends React.Component {
   render() {
@@ -19,10 +18,17 @@ class Rainbow extends React.Component {
 
         <div id="rainbow">
           {/* Your routes should go here */}
+          <Route path="/red" component={Red} />
+          <Route path="/red/orange" component={Orange} />
+          <Route path="/red/yellow" component={Yellow} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/blue/indigo" component={Indigo} />
+          <Route path="/violet" component={Violet} />
         </div>
       </div>
     );
   }
-};
+}
 
 export default Rainbow;
